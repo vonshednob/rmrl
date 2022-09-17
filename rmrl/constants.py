ef5c989774f7c4bedf71ac4c51727da4003796a9
Robert Labudda <dev+remove-if-not-spam@kakaomilchkuh.de>
2022-09-17 09:40:37 +0200

    xdg_data_home is not available in all version; fallback to hard-coded defaults but try hard first

--- rmrl/constants.py
+++ rmrl/constants.py
@@ -1,7 +1,15 @@
 from pathlib import Path
 import pkg_resources
 
-from xdg import xdg_data_home
+try:
+    from xdg import xdg_data_home
+except ImportError:
+    xdg_data_home = None
+
+try:
+    from xdg import BaseDirectory
+except ImportError:
+    BaseDirectory = None
 
 # From rcu.py, with comment
 # Todo: this should be based on the specific RM model
@@ -20,7 +28,12 @@ PDFWIDTH = DISPLAY['screenwidth'] * PTPERPX
 SPOOL_MAX = 10 * 1024 * 1024
 
 # TODO: parameterize
-TEMPLATE_PATH = xdg_data_home() / 'rmrl' / 'templates'
+XDG_DATA_HOME = Path().home() / '.local' / 'share'
+if xdg_data_home is not None:
+    XDG_DATA_HOME = Path(xdg_data_home())
+elif BaseDirectory is not None:
+    XDG_DATA_HOME = Path(BaseDirectory.xdg_data_home)
+TEMPLATE_PATH = XDG_DATA_HOME / 'rmrl' / 'templates'
 
 VERSION = pkg_resources.get_distribution('rmrl').version
 
